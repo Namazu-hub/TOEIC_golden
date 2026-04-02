b3afea0aa299218679d3c5cdf0bc14ed2374402f
Subaru <83542274+Namazu-hub@users.noreply.github.com>
2026-04-02 23:48:59 +0900

    Update data-600.js

--- js/data-600.js
+++ js/data-600.js
@@ -8,11 +8,75 @@ const data600 = [
         hint: "Let's try a______"
     },
     {
-        en: "ship", 
-        jp: "出荷する", 
+        en: "appointment", 
+        jp: "予約、約束", 
         cat: "600", 
-        phrase: "The order will ship tomorrow", 
-        phraseJp: "注文品は明日出荷されます",
-        hint: "The order will s______ tomorrow"
+        phrase: "make an appointment", 
+        phraseJp: "予約を取る",
+        hint: "make an a______"
+    },
+    {
+        en: "available", 
+        jp: "利用可能な、手が空いている", 
+        cat: "600", 
+        phrase: "I am available on Friday", 
+        phraseJp: "金曜日は都合がつきます",
+        hint: "I am a______ on Friday"
+    },
+    {
+        en: "inform", 
+        jp: "知らせる", 
+        cat: "600", 
+        phrase: "inform the staff", 
+        phraseJp: "スタッフに知らせる",
+        hint: "i______ the staff"
+    },
+    {
+        en: "submit", 
+        jp: "提出する", 
+        cat: "600", 
+        phrase: "submit a report", 
+        phraseJp: "報告書を提出する",
+        hint: "s______ a report"
+    },
+    {
+        en: "location", 
+        jp: "場所、立地", 
+        cat: "600", 
+        phrase: "a new location", 
+        phraseJp: "新しい場所",
+        hint: "a new l______"
+    },
+    {
+        en: "provide", 
+        jp: "提供する", 
+        cat: "600", 
+        phrase: "provide a service", 
+        phraseJp: "サービスを提供する",
+        hint: "p______ a service"
+    },
+    {
+        en: "instead", 
+        jp: "代わりに", 
+        cat: "600", 
+        phrase: "use this instead", 
+        phraseJp: "代わりにこれを使って",
+        hint: "use this i______"
+    },
+    {
+        en: "prepare", 
+        jp: "準備する", 
+        cat: "600", 
+        phrase: "prepare a document", 
+        phraseJp: "書類を準備する",
+        hint: "p______ a document"
+    },
+    {
+        en: "require", 
+        jp: "必要とする", 
+        cat: "600", 
+        phrase: "require extra time", 
+        phraseJp: "追加時間を必要とする",
+        hint: "r______ extra time"
     }
 ];
