7cfa531137d57f7d8ec8bfd1d879fe57e1bc9098
Subaru <83542274+Namazu-hub@users.noreply.github.com>
2026-04-02 23:49:19 +0900

    Update data-730.js

--- js/data-730.js
+++ js/data-730.js
@@ -1,5 +1,37 @@
 const data730 = [
     {
+        en: "representative", 
+        jp: "担当者、代表者", 
+        cat: "730", 
+        phrase: "a sales representative", 
+        phraseJp: "営業担当者",
+        hint: "a sales r______"
+    },
+    {
+        en: "upcoming", 
+        jp: "今度の、間近に迫った", 
+        cat: "730", 
+        phrase: "the upcoming event", 
+        phraseJp: "今度のイベント",
+        hint: "the u______ event"
+    },
+    {
+        en: "authorized", 
+        jp: "公認の、権限を与えられた", 
+        cat: "730", 
+        phrase: "an authorized dealer", 
+        phraseJp: "公認ディーラー",
+        hint: "an a______ dealer"
+    },
+    {
+        en: "confirm", 
+        jp: "確認する", 
+        cat: "730", 
+        phrase: "confirm the reservation", 
+        phraseJp: "予約を確認する",
+        hint: "c______ the reservation"
+    },
+    {
         en: "conduct", 
         jp: "行う、実施する", 
         cat: "730", 
@@ -8,11 +40,43 @@ const data730 = [
         hint: "c______ a survey"
     },
     {
-        en: "additional", 
-        jp: "追加の", 
+        en: "implementation", 
+        jp: "実施、導入", 
         cat: "730", 
-        phrase: "at no additional cost", 
-        phraseJp: "追加料金なしで",
-        hint: "at no a______ cost"
+        phrase: "implementation of the plan", 
+        phraseJp: "計画の実施",
+        hint: "i______ of the plan"
+    },
+    {
+        en: "comprehensive", 
+        jp: "包括的な、詳細な", 
+        cat: "730", 
+        phrase: "a comprehensive list", 
+        phraseJp: "包括的なリスト",
+        hint: "a c______ list"
+    },
+    {
+        en: "distinguished", 
+        jp: "際立った、優れた", 
+        cat: "730", 
+        phrase: "a distinguished guest", 
+        phraseJp: "際立ったゲスト（貴賓）",
+        hint: "a d______ guest"
+    },
+    {
+        en: "negotiate", 
+        jp: "交渉する", 
+        cat: "730", 
+        phrase: "negotiate a contract", 
+        phraseJp: "契約を交渉する",
+        hint: "n______ a contract"
+    },
+    {
+        en: "substantial", 
+        jp: "かなりの、実質的な", 
+        cat: "730", 
+        phrase: "a substantial increase", 
+        phraseJp: "かなりの増加",
+        hint: "a s______ increase"
     }
 ];
