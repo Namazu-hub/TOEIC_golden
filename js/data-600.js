94823e72015e60363a1446a32bcffd50fcd36a33
Subaru <83542274+Namazu-hub@users.noreply.github.com>
2026-04-02 15:53:11 +0900

    Create data-600.js

---
+++ js/data-600.js
@@ -0,0 +1,18 @@
+const data600 = [
+    {
+        en: "anyway", 
+        jp: "とにかく", 
+        cat: "600", 
+        phrase: "Let's try anyway", 
+        phraseJp: "とにかくやってみよう",
+        hint: "Let's try a______"
+    },
+    {
+        en: "ship", 
+        jp: "出荷する", 
+        cat: "600", 
+        phrase: "The order will ship tomorrow", 
+        phraseJp: "注文品は明日出荷されます",
+        hint: "The order will s______ tomorrow"
+    }
+];
