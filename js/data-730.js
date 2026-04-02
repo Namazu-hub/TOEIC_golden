ab19b787fe9b2779f47e72466570ad5d72eca3df
Subaru <83542274+Namazu-hub@users.noreply.github.com>
2026-04-02 15:54:08 +0900

    Create data-730.js

---
+++ js/data-730.js
@@ -0,0 +1,18 @@
+const data730 = [
+    {
+        en: "conduct", 
+        jp: "行う、実施する", 
+        cat: "730", 
+        phrase: "conduct a survey", 
+        phraseJp: "アンケート調査を行う",
+        hint: "c______ a survey"
+    },
+    {
+        en: "additional", 
+        jp: "追加の", 
+        cat: "730", 
+        phrase: "at no additional cost", 
+        phraseJp: "追加料金なしで",
+        hint: "at no a______ cost"
+    }
+];
